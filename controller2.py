8cc2ddbd5636a9db6d5113cbe1c95373dac0b7a4
jlr84 <james@7spartan.com>
2016-10-11 17:16:14 -0400

    Added new temp controllers

---
+++ controller2.py
@@ -0,0 +1,54 @@
+import socket
+import sys
+
+def my_connect_fn():
+    # Create a TCP/IP socket
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    # Bind the socket to the port
+    server_address = ('localhost', 10000)
+    print >>sys.stderr, 'starting up on %s port %s' % server_address
+    sock.bind(server_address)
+
+    # Listen for incoming connections
+    sock.listen(1)
+
+    while True:
+        # Wait for a connection
+        print >>sys.stderr, 'waiting for a connection'
+        connection, client_address = sock.accept()
+
+        try:
+            print >>sys.stderr, 'connection from', client_address
+
+            # Receive the data in small chunks and retransmit it
+            while True:
+                data = connection.recv(16)
+                print >>sys.stderr, 'received "%s"' % data
+                if data:
+                    print >>sys.stderr, 'sending data back to the client'
+                    connection.sendall(data)
+                else:
+                    print >>sys.stderr, 'no more data from', client_address
+                    break
+            
+        finally:
+            # Clean up the connection
+            connection.close()
+
+def my_quit_fn():
+    raise SystemExit
+
+def invalid():
+    print "INVALID CHOICE!"
+
+#def my_menu_fn():
+menu = {"1":("Connect",my_connect_fn),
+        "2":("Quit",my_quit_fn)
+       }
+
+for key in sorted(menu.keys()):
+    print key+":" + menu[key][0]
+
+ans = raw_input("Make a Choice\n>>> ")
+menu.get(ans,[None,invalid])[1]()
