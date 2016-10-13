648ae9c445439d124c3ae7c836d54f07979dd84b
jlr84 <james@7spartan.com>
2016-10-13 12:45:57 -0400

    Controller detects HostIP and starts based on menu

--- controller.py
+++ controller.py
@@ -1,37 +1,73 @@
 import socket
 import sys
 
-# Create a TCP/IP socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+def getMyIP():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8",53))
+    ipAdd = s.getsockname()[0]
+    s.close()
+    return ipAdd
 
-# Bind the socket to the port
-server_address = ('localhost', 10000)
-print >>sys.stderr, 'starting up on %s port %s' % server_address
-sock.bind(server_address)
+def runServer():
+    # Determine IP Address
+    ipAddress = getMyIP()
 
-# Listen for incoming connections
-sock.listen(1)
+    # Create a TCP/IP socket
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-while True:
-    # Wait for a connection
-    print >>sys.stderr, 'waiting for a connection'
-    connection, client_address = sock.accept()
+    # Bind the socket to the port
+    server_address = (ipAddress, 10000)
+    print >>sys.stderr, 'starting up on %s port %s' % server_address
+    sock.bind(server_address)
 
-    try:
-        print >>sys.stderr, 'connection from', client_address
+    # Listen for incoming connections
+    sock.listen(1)
 
-        # Receive the data in small chunks and retransmit it
-        while True:
-            data = connection.recv(16)
-            print >>sys.stderr, 'received "%s"' % data
-            if data:
-                print >>sys.stderr, 'sending data back to the client'
-                connection.sendall(data)
-            else:
-                print >>sys.stderr, 'no more data from', client_address
-                break
-            
-    finally:
-        # Clean up the connection
-        connection.close()
+    while True:
+        # Wait for a connection
+        print >>sys.stderr, 'waiting for a connection'
+        connection, client_address = sock.accept()
+
+        try:
+            print >>sys.stderr, 'connection from', client_address
+
+            # Receive the data in small chunks and retransmit it
+            while True:
+                data = connection.recv(16)
+                print >>sys.stderr, 'received "%s"' % data
+                if data:
+                    print >>sys.stderr, 'sending data back to the client'
+                    connection.sendall(data)
+                else:
+                    print >>sys.stderr, 'no more data from', client_address
+                    break
+        finally:
+            # Clean up the connection
+            connection.close()
+
+def my_quit_fn():
+    raise SystemExit
+
+def invalid():
+    print "INVALID CHOICE!"
+
+def myMenu():
+    m = {"1":("Connect",runServer),
+         "2":("Quit",my_quit_fn)
+           }
+    for key in sorted(m.keys()):
+        print key+":" + m[key][0]
+    ans = raw_input("Make a Choice\n>>> ")
+    m.get(ans,[None,invalid])[1]()
+
+##################
+# Start of Program
+##################
+
+print "Detecting IP Address..."
+myIP = getMyIP()
+print "Host IP: %s\n" % (myIP)
+
+# Run Menu for user
+myMenu()
 
